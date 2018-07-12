08f18f35745d888062ef1bc2cbf12689dc06e23c
Lex. Huang <531971909@qq.com>
2018-07-12 10:07:50 +0800

    fix(CallCtrlPage): fix onhold list criteria

--- packages/ringcentral-widgets/containers/CallCtrlPage/index.js
+++ packages/ringcentral-widgets/containers/CallCtrlPage/index.js
@@ -455,7 +455,9 @@ function mapToFunctions(_, {
           const session = webphone._sessions.get(sessionId);
           conferenceCall.setMergeParty({ from: session });
         }
-        if (callMonitor.activeOnHoldCalls.length) {
+        const outBoundOnholdCalls = callMonitor.activeOnHoldCalls
+          .filter(call => call.direction === callDirections.outbound);
+        if (outBoundOnholdCalls.length) {
           // goto 'calls on hold' page
           routerInteraction
             .push(`/conferenceCall/callsOnhold/${sessionData.fromNumber}/${sessionData.id}`);
